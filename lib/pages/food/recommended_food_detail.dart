6c0f23cc8fe6222e7b7e3215ac8bd517394accd8
Enes AGIR <enes.agir.7@gmail.com>
2022-03-02 12:45:47 +0100

    food detail page icon

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
 import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/exandable_text_widget.dart';
 
 class RecommendedFoodDetail extends StatelessWidget {
   const RecommendedFoodDetail({ Key? key }) : super(key: key);
@@ -10,15 +12,31 @@ class RecommendedFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            toolbarHeight: 65,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.clear),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
             bottom: PreferredSize(
-                preferredSize: Size.fromHeight(20),child: Container(
-                  color: Colors.white,
+                preferredSize: Size.fromHeight(20),
+                child: Container(
                   child: Center(child: BigText(size: Dimensions.font26, text: "Chinese Side")),
                   width: double.maxFinite,
                   padding: EdgeInsets.only(top: 5, bottom: 10,),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(Dimensions.radius20),
+                      topRight: Radius.circular(Dimensions.radius20),
+                    )
+                  ),
                 ),
               ),
             pinned: true,
@@ -29,10 +47,40 @@ class RecommendedFoodDetail extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: Text(
-              "biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin"
-              ),
+            child: Column(
+              children: [
+                Container(
+                  child: ExpandableTextWidget(text:
+                    "biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesnin biryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesninbiryani is the bes food for dinner besiktas ben sesnin"),
+                  margin: EdgeInsets.only(left: Dimensions.width20, right: Dimensions.width20),
+                )
+              ],
+            ),
           ),
+        ],
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20*2.5,
+              right: Dimensions.width20*2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                  iconsize: Dimensions.iconSize24, iconcolor: Colors.white, 
+                  backgroundColor: AppColors.mainColor, icon: Icons.remove),
+                AppIcon(
+                  iconsize: Dimensions.iconSize24,iconcolor: Colors.white, 
+                  backgroundColor: AppColors.mainColor, icon: Icons.add),
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -6,21 +6,21 @@ class AppIcon extends StatelessWidget {
   final Color backgroundColor;
   final Color iconcolor;
   final double size;
-  AppIcon({Key? key,required this.icon, this.backgroundColor=const Color(0xFFfcf4e4), this.iconcolor=const Color(0xFF756d54), this.size=40}) : super(key: key);
+  final double iconsize;
+  AppIcon({Key? key, required this.icon, this.backgroundColor = const Color(0xFFfcf4e4), 
+            this.iconcolor = const Color(0xFF756d54), 
+            this.size = 40,this.iconsize=16}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: size,
       height: size,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(size/2),
-        color: backgroundColor
-      ),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(size / 2), color: backgroundColor),
       child: Icon(
         icon,
         color: iconcolor,
-        size: Dimensions.iconSize16,
+        size: iconsize,
       ),
     );
   }
